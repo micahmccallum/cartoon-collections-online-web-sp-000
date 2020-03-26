ab280a14d108a6191494714cd954b79efc820a09
Micah McCallum <micahmccallum@outlook.com>
2020-03-26 23:08:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+
+  array.each_with_index do |name, index|
+    puts "#{array[index]}"
+  end
+
 end
 
 def summon_captain_planet# code an argument here

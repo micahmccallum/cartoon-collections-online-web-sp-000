a02c4cf6e8d052a022d39d88ac84caad267ec509
Micah McCallum <micahmccallum@outlook.com>
2020-03-26 23:18:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,11 +5,13 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.map { |item| item.capitalize + "!"}  
+  array.map { |item| item.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.include? do |item|
+    item.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here

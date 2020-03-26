3ceb268ff487370396320ee3f12f74cc77fba123
Micah McCallum <micahmccallum@outlook.com>
2020-03-26 23:24:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.include? { |item|  item.length > 4 }
+  array.any? { |item|  item.length > 4 }
 end
 
 def find_the_cheese# code an argument here

f5bc23188f54508b4c3bb22d5f83b3e3ff67d166
Micah McCallum <micahmccallum@outlook.com>
2020-03-27 00:17:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,9 @@ def long_planeteer_calls(array)
   array.any? { |item|  item.length > 4 }
 end
 
-def find_the_cheese
-  array.find {}
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese|
+    array.find { |item| item == cheese }
+  end
 end
